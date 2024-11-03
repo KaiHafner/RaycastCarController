811b7158ff2571ee8693b0928a7c346df5823e49
Kai <kai.hafner@outlook.com>
2024-11-03 23:06:31 +0000

    Initial commit

---
+++ Source/RaycastCarController/RaycastCarController.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "RaycastCarController.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, RaycastCarController, "RaycastCarController" );
